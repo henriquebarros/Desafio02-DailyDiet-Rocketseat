84929bfe768cf42abb9ad3d75d330a294f7f52b3
henriquebarros <henriquebarros@live.com>
2024-01-31 21:22:47 -0300

    implementing lib zod for body data validation

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,7 +1,33 @@
 import { FastifyInstance } from "fastify"
+import { z } from "zod"
+
 
 export async function mealsRoutes(app:FastifyInstance){
-    app.get('/', async (request, reply)=>{
-        reply.status(200).send('Ok')
+    app.post('/', async (request, reply)=>{
+        const createMealBodySchema = z.object({
+            name: z.string(),
+            date_meals: z.coerce.date().refine((data)=>{
+                console.log(data > new Date())
+                return data < new Date()
+            }, {
+                message: 'Invalid date'
+            }),
+            within_diet: z.boolean()
+        })
+
+
+        const { name, date_meals, within_diet } = createMealBodySchema.parse( request.body )
+
+        console.log(name, date_meals, within_diet )
     })
 }
+
+/**
+ *         table.uuid('id').primary()
+        table.text('name').notNullable()
+        table.timestamp('date_meals').defaultTo(knex.fn.now()).notNullable()
+        table.boolean('within_diet').defaultTo(true).notNullable()
+        table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+        table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
+        table.uuid('session_id').after('id').index()
+ */
