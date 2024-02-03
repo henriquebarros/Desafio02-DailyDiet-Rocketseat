bb56a2f1690a89d37b63222608ab5dd2522bec75
henriquebarros <henriquebarros@live.com>
2024-02-03 17:19:41 -0300

    preparing the environment for project testing

---
+++ test/users.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+import { app } from '../src/app'
+
+describe('Users routes', () => {
+  it('should add 1 + 1 to equal 2', () => {
+    expect(1 + 1).toEqual(2)
+  })
+})
