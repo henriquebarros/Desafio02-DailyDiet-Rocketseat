e7fc723955c930d087c3917a2a0efca0b6c03245
henriquebarros <henriquebarros@live.com>
2024-02-05 18:17:50 -0300

    Test: should be able to list all of user is meals

--- test/meals.spec.ts
+++ test/meals.spec.ts
@@ -1,4 +1,4 @@
-import { describe, beforeAll, afterAll, it, beforeEach } from 'vitest'
+import { describe, beforeAll, afterAll, it, beforeEach, expect } from 'vitest'
 import request from 'supertest'
 import { execSync } from 'node:child_process'
 import { app } from '../src/app'
@@ -34,5 +34,47 @@ describe('Meals routes', () => {
         within_diet: false,
       })
       .expect(201)
+  })
+
+  it('should be able to list all of user is meals', async () => {
+    const createUser = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'Henrique Barros',
+        email: 'henriquebarros@live.com',
+      })
+      .expect(201)
+
+    const cookies = createUser.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Café da manhã',
+        description: 'Pão de Queijo Vegano',
+        date_meals: '2024-01-25T20:32:10.279Z',
+        within_diet: true,
+      })
+      .expect(201)
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies)
+      .send({
+        name: 'Creme de Camarão',
+        description: 'Risoto cremoso com camarões suculentos',
+        date_meals: '2024-01-25T20:32:10.279Z',
+        within_diet: false,
+      })
+      .expect(201)
+
+    const listMealsResponse = await request(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+      .expect(200)
+    console.log(listMealsResponse.body.meals)
+    expect(listMealsResponse.body.meals[0].name).toBe('Café da manhã')
+    expect(listMealsResponse.body.meals[1].name).toBe('Creme de Camarão')
   })
 })
